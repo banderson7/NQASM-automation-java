31104369067ac6e0bf6253faaf183836585b07a4
Bill Anderson <bianders@bianders-osx.local>
2018-09-26 20:27:38 -0500

    pom updated for compile fix, Sign Up page tests added

--- src/main/java/SignUpPage.java
+++ src/main/java/SignUpPage.java
@@ -6,6 +6,16 @@ public class SignUpPage {
     private final String url;
 
     private By header = By.className("display-4");
+    private By subHeader = By.className("lead");
+    private By nameField = By.id("name");
+    private By nameError = By.id("nameError");
+    private By emailField = By.id("email");
+    private By emailError = By.id("emailError");
+    private By passwordField = By.id("password");
+    private By passwordError = By.id("passwordError");
+    private By password2Field = By.id("password2");
+    private By password2Error = By.id("password2Error");
+    private By submitButton = By.xpath("//input[@type='submit']");
 
     public String getPageUrl(){ return url; }
 
@@ -17,4 +27,25 @@ public class SignUpPage {
     public String getHeaderText() {
         return driver.findElement(header).getText();
     }
+
+    public String getSubHeaderText() { return driver.findElement(subHeader).getText(); }
+
+    public String getNameFieldText() { return driver.findElement(nameField).getAttribute("value");}
+
+    public String getNameErrorText() { return driver.findElement(nameError).getText(); }
+
+    public String getEmailErrorText() { return driver.findElement(emailError).getText(); }
+
+    public String getPasswordErrorText() { return driver.findElement(passwordError).getText(); }
+
+    public String getPassword2ErrorText() { return driver.findElement(password2Error).getText(); }
+
+    public void clickSubmit(){
+        driver.findElement(submitButton).click();
+    }
+
+    public void enterName(String text){
+        driver.findElement(nameField).sendKeys(text);
+    }
+
 }
--- src/test/java/SignUpPageTest.java
+++ src/test/java/SignUpPageTest.java
@@ -7,6 +7,13 @@ public class SignUpPageTest {
     private WebDriver driver;
     private SignUpPage signUpPage;
 
+    private final String headerText = "Sign Up";
+    private final String subHeaderText = "Create your NQA Social Media account";
+    private final String nameIsRequiredError = "Name is required";
+    private final String emailIsRequiredError = "Email is required";
+    private final String passwordIsRequiredError = "Password is required";
+    private final String password2IsRequiredError = "Confirm password is required";
+    private final String nameInvalidLengthError = "Name must be between 2 and 30 characters";
 
     @BeforeClass
     public void setUp(){
@@ -22,12 +29,37 @@ public class SignUpPageTest {
         driver.get(signUpPage.getPageUrl());
     }
 
+    @Test
     public void assertHeader(){
-
+        Assert.assertEquals(signUpPage.getHeaderText(), this.headerText);
     }
 
+    @Test (enabled=false)
     public void assertSubHeader(){
+        Assert.assertEquals(signUpPage.getSubHeaderText(), this.subHeaderText);
+    }
 
+    @Test (description = "Click submit and verify all error messages display")
+    public void assertEmptySubmit(){
+        signUpPage.clickSubmit();
+        Assert.assertEquals(signUpPage.getNameErrorText(), this.nameIsRequiredError);
+        Assert.assertEquals(signUpPage.getEmailErrorText(), this.emailIsRequiredError);
+        Assert.assertEquals(signUpPage.getPasswordErrorText(), this.passwordIsRequiredError);
+        Assert.assertEquals(signUpPage.getPassword2ErrorText(), this.password2IsRequiredError);
+    }
+
+    @Test
+    public void assertNameTooShortSubmit(){
+        signUpPage.enterName("a");
+        signUpPage.clickSubmit();
+        Assert.assertEquals(signUpPage.getNameErrorText(), this.nameInvalidLengthError);
+    }
+
+    @Test
+    public void assertNameTooLongSubmit(){
+        signUpPage.enterName("abcdefghijklmnopqrstuvwxyzqwert");
+        signUpPage.clickSubmit();
+        Assert.assertEquals(signUpPage.getNameErrorText(), this.nameInvalidLengthError);
     }
 
     @AfterClass
